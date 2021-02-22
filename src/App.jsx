9f60be2701f1124340010eebdb409745a302f10f
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-22 16:08:59 -0500

    Added componentDidMount to set a Random Color every time page is loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,68 @@
 import React, { Component } from 'react'
 
-// state = {
-//   color:
-// }
-
 export class App extends Component {
+  state = {
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+  }
+
+  componentDidMount() {
+    this.setState({
+      hue: Math.floor(Math.random() * (360 - 0)) + 0,
+      saturation: Math.floor(Math.random() * (100 - 0)) + 0,
+      lightness: Math.floor(Math.random() * (100 - 0)) + 0,
+    })
+  }
+
   render() {
     return (
       <div>
-        <header>
+        <header
+          style={{
+            backgroundColor: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+          }}
+        >
           <h1>Color Picker</h1>
         </header>
         <main>
+          <h2>
+            This color is: hsl({this.state.hue}, {this.state.saturation}%,
+            {this.state.lightness}%)
+          </h2>
+          <button onClick={this.randomColor}>Random Color</button>
           <section>
             <label htmlFor="Hue">Hue: </label>
-            <input type="range" min="0" max="360" name="Hue"></input>
+            <input
+              type="range"
+              min="0"
+              max="360"
+              onInput={this.updateHue}
+              value={this.state.hue}
+              name="Hue"
+            ></input>
           </section>
           <section>
             <label htmlFor="Saturation">Saturation: </label>
-            <input type="range" min="0" max="100" name="Saturation"></input>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              onInput={this.updateSaturation}
+              value={this.state.saturation}
+              name="Saturation"
+            ></input>
           </section>
           <section>
             <label htmlFor="Lightness">Lightness: </label>
-            <input type="range" min="0" max="100" name="Lightness"></input>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              onInput={this.updateLightness}
+              value={this.state.lightness}
+              name="Lightness"
+            ></input>
           </section>
         </main>
         <footer>This Color Picker App was created by Mandy Wade</footer>
